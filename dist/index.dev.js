76f0593de1e7131959f8a3e6ab9049f8d8e149b9
admin <rawiamhamdi@yahoo.fr>
2022-06-23 15:36:09 +0200

    commited

---
+++ dist/index.dev.js
@@ -0,0 +1,104 @@
+"use strict";
+
+var img;
+var upload;
+var saturate;
+var contrast;
+var brightness;
+var sepia;
+var grayscale;
+var blur;
+var hueRotate;
+var download;
+var reset;
+var imgBox;
+
+window.onload = function () {
+  img = document.getElementById("img");
+  upload = document.getElementById("upload");
+  saturate = document.getElementById("saturate");
+  contrast = document.getElementById("contrast");
+  brightness = document.getElementById("brightness");
+  sepia = document.getElementById("sepia");
+  grayscale = document.getElementById("grayscale");
+  blur = document.getElementById("blur");
+  hueRotate = document.getElementById("hue-rotate");
+  download = document.getElementById("download");
+  reset = document.querySelector("span");
+  imgBox = document.querySelector(".imgbox");
+  download.style.display = "none";
+  reset.style.display = "none";
+  imgBox.style.display = "none";
+
+  upload.onchange = function () {
+    alert("upload Button change");
+    download.style.display = "block";
+    reset.style.display = "block";
+    imgBox.style.display = "block";
+    var file = new FileReader();
+    file.readAsDataURL(upload.files[0]);
+
+    file.onload = function () {
+      img.src = file.result;
+    };
+
+    console.log(img);
+  };
+};
+
+function resetValue() {
+  img.style.filter = "none";
+  saturate.value = "100";
+  contrast.value = "100";
+  brightness.value = "100";
+  sepia.value = "0";
+  grayscale.value = "0";
+  blur.value = "0";
+  hueRotate.value = "0";
+}
+
+;
+var data = document.querySelectorAll("[type = range]");
+var objRange = {};
+
+for (var i = 0; i < data.length; i++) {
+  objRange[data[i].id] = data[i].value;
+} // let keysOfObjRange = Object.keys(objRange);
+
+
+var _loop = function _loop(_i) {
+  data[_i].onchange = function () {
+    objRange[data[_i].id] = document.getElementById("".concat(data[_i].id)).value;
+    img.style.filter = "\n            saturate(".concat(objRange.saturate, "%)\n            contrast(").concat(objRange.contrast, "%)\n            brightness(").concat(objRange.brightness, "%)\n            sepia(").concat(objRange.sepia, "%)\n            grayscale(").concat(objRange.grayscale, ")\n            blur(").concat(objRange.blur, "px)\n            hue-rotate(").concat(objRange["hue-rotate"], "deg)\n            ");
+    /* ${keysOfObjRange[0]}(${objRange[keysOfObjRange[0]]}%)
+    ${keysOfObjRange[1]}(${objRange[keysOfObjRange[1]}%)
+    ${keysOfObjRange[2]}(${objRange[keysOfObjRange[2]]}%)
+    ${keysOfObjRange[3]}(${objRange[keysOfObjRange[3]]}%)
+    ${keysOfObjRange[4]}(${objRange[keysOfObjRange[4]]})
+    ${keysOfObjRange[5]}(${objRange[keysOfObjRange[5]]}px)
+    ${keysOfObjRange[6]}(${objRange[keysOfObjRange[6]]}deg) */
+  };
+};
+
+for (var _i = 0; _i < data.length; _i++) {
+  _loop(_i);
+}
+/*let filters=document.querySelectorAll("ul li input");
+
+for(let i = 0; i < filters.length; i++){
+
+}*/
+// filters.forEach(inputs =>{
+// filter.addEventListener(`input`,function(){
+//     console.log('Something changes',saturate.value);
+//     img.style.filter = `
+//     saturate(${saturate.value}%)
+//     contrast(${contrast.value}%)
+//     brightness(${brightness.value}%)
+//     sepia(${sepia.value}%)
+//     grayscale (${grayscale.value})
+//     blur(${blur.value}px)
+//     hue-rotate(${hueRotate.value}deg) 
+//     `
+// })
+// });
---
+++ index.js
@@ -0,0 +1,127 @@
+
+let img
+let upload
+let saturate
+let contrast
+let brightness
+let sepia
+let grayscale
+let blur
+let hueRotate
+let download 
+let reset
+let imgBox
+
+
+window.onload = () => {
+    img=document.getElementById(`img`)
+    upload=document.getElementById(`upload`)
+    saturate=document.getElementById(`saturate`)
+    contrast=document.getElementById(`contrast`)
+    brightness=document.getElementById(`brightness`)
+    sepia=document.getElementById(`sepia`)
+    grayscale=document.getElementById(`grayscale`)
+    blur=document.getElementById(`blur`)
+    hueRotate=document.getElementById(`hue-rotate`)
+    download = document.getElementById(`download`)
+    reset=document.querySelector(`span`)
+    imgBox=document.querySelector(`.imgbox`)
+    
+    
+    download.style.display=`none`;
+    reset.style.display=`none`;
+    imgBox.style.display=`none`;
+
+
+    upload.onchange = () => {
+        alert("upload Button change")
+        download.style.display=`block`;
+        reset.style.display=`block`;
+        imgBox.style.display=`block`;
+        let file=new FileReader();
+        file.readAsDataURL(upload.files[0]);
+        file.onload=function(){
+            img.src=file.result;
+        }
+        console.log(img);  
+    }
+  
+    
+}
+function resetValue(){
+    img.style.filter=`none`;
+    saturate.value=`100`;
+    contrast.value=`100`;
+    brightness.value=`100`;
+    sepia.value=`0`;
+    grayscale.value=`0`;
+    blur.value=`0`;
+    hueRotate.value=`0`;
+};
+
+
+
+
+let data = document.querySelectorAll("[type = range]");
+let objRange = {
+
+}
+
+for(let i = 0; i < data.length; i++){
+    objRange[data[i].id] = data[i].value;
+}
+
+// let keysOfObjRange = Object.keys(objRange);
+
+for(let i = 0; i < data.length; i++){
+
+    
+    data[i].onchange = () => {
+        
+        objRange[data[i].id] = document.getElementById(`${data[i].id}`).value;
+
+
+        img.style.filter = `
+            saturate(${objRange.saturate}%)
+            contrast(${objRange.contrast}%)
+            brightness(${objRange.brightness}%)
+            sepia(${objRange.sepia}%)
+            grayscale(${objRange.grayscale})
+            blur(${objRange.blur}px)
+            hue-rotate(${objRange["hue-rotate"]}deg)
+            `
+            /* ${keysOfObjRange[0]}(${objRange[keysOfObjRange[0]]}%)
+            ${keysOfObjRange[1]}(${objRange[keysOfObjRange[1]}%)
+            ${keysOfObjRange[2]}(${objRange[keysOfObjRange[2]]}%)
+            ${keysOfObjRange[3]}(${objRange[keysOfObjRange[3]]}%)
+            ${keysOfObjRange[4]}(${objRange[keysOfObjRange[4]]})
+            ${keysOfObjRange[5]}(${objRange[keysOfObjRange[5]]}px)
+            ${keysOfObjRange[6]}(${objRange[keysOfObjRange[6]]}deg) */
+        
+    } 
+}
+
+/*let filters=document.querySelectorAll("ul li input");
+
+for(let i = 0; i < filters.length; i++){
+
+}*/
+
+// filters.forEach(inputs =>{
+// filter.addEventListener(`input`,function(){
+//     console.log('Something changes',saturate.value);
+//     img.style.filter = `
+    
+//     saturate(${saturate.value}%)
+//     contrast(${contrast.value}%)
+//     brightness(${brightness.value}%)
+//     sepia(${sepia.value}%)
+//     grayscale (${grayscale.value})
+//     blur(${blur.value}px)
+//     hue-rotate(${hueRotate.value}deg) 
+
+
+//     `
+
+// })
+// });
